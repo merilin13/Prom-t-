1fd2c279f07457ed0349aff6afe039d5a31583ff
merilin13 <150146754+merilin13@users.noreply.github.com>
2023-11-23 15:00:53 +0200

    Add files via upload

---
+++ ifandelsecolor/ifandelsecolor/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ifandelsecolor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta nimi");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("nimi on sisestatud ja nüüd hakkab kontroll");
+            if(name !="")
+            {
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.BackgroundColor= ConsoleColor.Red;
+
+                Console.WriteLine("Tere, " + name + " ! " );
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else 
+            { 
+
+            Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor= ConsoleColor.Red;
+
+                Console.WriteLine("Nime ei sisestatud.");
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+        }
+    }
+}
