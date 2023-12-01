3b61f81243e3b70521e92901939107c46291f599
merilin13 <150146754+merilin13@users.noreply.github.com>
2023-12-01 13:49:34 +0200

    Add files via upload

---
+++ switch/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Switch;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Sisesta täht ja vajuta enter");
+
+        string ch = Console.ReadLine();
+
+        switch (ch) 
+        {
+            case "a":
+                Console.WriteLine("Vokaal a");
+                break;
+
+            case "e":
+                Console.WriteLine("Vokaal e");
+                break;
+
+            case "i":
+                Console.WriteLine("Vokaal i");
+                break;
+
+                default:
+                Console.WriteLine("Ei ole vokaal ehk täishäälik");
+                break;
+
+
+        }
+    }
+}
