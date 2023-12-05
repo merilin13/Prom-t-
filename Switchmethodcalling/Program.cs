4fe4b22649bd2d86f55c9e92b6249b97d7d955d0
merilin13 <150146754+merilin13@users.noreply.github.com>
2023-12-05 15:48:11 +0200

    Add files via upload

---
+++ Switchmethodcalling/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Switchmethodcalling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string topic, category;
+
+            topic = Console.ReadLine();
+
+            switch (topic)
+            {
+                case "kollane":
+                case "punane":
+                case "valge":
+                    
+
+                    Main1();
+                    break;
+
+                case "roosa":
+                case "sinine":
+                case "pruun":
+                    
+                    Main2();
+                    break;
+
+                case "roheline":
+                case "lilla":
+                case "mere sinine":
+                case "okse":
+                   
+                    Main3();
+                    break;
+
+                default:
+
+                    category = "sisesta mingi teine värv";
+                    break;
+                  
+
+            }
+        }
+        static void Main1()
+        {
+            Console.WriteLine("lahedad värvid");
+        }
+        static void Main2()
+        {
+            Console.WriteLine("eriti lahedad värvid");
+        }
+        static void Main3()
+        {
+            Console.WriteLine("arvo pluusi värvid");
+        }
+    }
+}
