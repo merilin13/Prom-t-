282e2ce2898dba0f5c701f6666fe360c16dbb9f9
merilin13 <150146754+merilin13@users.noreply.github.com>
2023-12-05 14:51:57 +0200

    Add files via upload

---
+++ cases/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace cases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string topic, category;
+
+            topic = "Jump Statements";
+
+            switch (topic)
+            {
+                case "Intro to C#":
+                case "Variables":
+                case "Data Types":
+
+                    category = "Basic";
+                    break;
+
+                case "Loops":
+                case "If Statements":
+                case "Jump Statements":
+
+                    category = "Control flow";
+                    break;
+
+                case "Class & objects":
+                case "Inheritance":
+                case "Constructors":
+
+                    category = "OOP-S Consept";
+                    break;
+
+                default:
+                    category = "UNKNOWN"; 
+                    break;
+            }
+            Console.WriteLine("category is " + category);
+        }
+    }
+}
