cd41a24e3000540d4ab6bb48af168cd90c603893
merilin13 <150146754+merilin13@users.noreply.github.com>
2023-12-07 15:48:09 +0200

    Add files via upload

---
+++ Switch with voo/Switch2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Switch2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta number string andmetüübina");
+
+            string number = Console.ReadLine();
+
+            switch (number) 
+            {
+                case "1":                 
+                case "2":
+                    Console.WriteLine("valisid ühe või kahe");
+                    break;
+                default:
+                    Console.WriteLine("Valisid midagi muud");
+                    break;
+            }
+        }
+    }
+}
