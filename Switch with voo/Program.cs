6be4306f4991872d61891136a37d9cba6b5b1e57
merilin13 <150146754+merilin13@users.noreply.github.com>
2023-12-07 15:32:47 +0200

    Add files via upload

---
+++ Switch with voo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Switch_with_voo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
