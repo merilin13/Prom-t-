1a7765e55d4c52a60d8a83809ddce5f2c021c772
merilin13 <150146754+merilin13@users.noreply.github.com>
2023-12-11 14:10:37 +0200

    ring22

---
+++ whiledo/Whiledoshapes/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Whiledoshapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KUJUU");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+
+            while (i <= n) 
+            {
+                Console.WriteLine();
+                int j = 1;
+                while(j <= i)
+                {
+                    Console.Write(j + " ");
+                    j++;
+                }
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
