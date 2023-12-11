dfbf9cb172d47e4f39cffb3426454a90e0d0806a
merilin13 <150146754+merilin13@users.noreply.github.com>
2023-12-11 12:57:52 +0200

    Add files via upload

---
+++ whiledo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace whiledo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While ja do");
+            int number = 1;
+            do
+            {
+                Console.Write(number);
+                number++;
+            }
+            while (number <= 5000);
+        }
+    }
+}
