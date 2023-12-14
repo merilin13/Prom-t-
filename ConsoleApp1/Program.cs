42ae70da31f06a9a6af12753764bbb07af47ff59
merilin13 <150146754+merilin13@users.noreply.github.com>
2023-12-14 15:09:38 +0200

    arvutuseddd

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sulgude kasutamine");
+            Console.WriteLine("Kui palju on : 3+ 8 / (4-2) * 4 = ?");
+
+            double calculation1 = 3 + 8 / (4 - 2) * 4;
+
+            Console.WriteLine("vastus " +  calculation1);
+        }
+    }
+}
