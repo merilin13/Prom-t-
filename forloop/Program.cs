2ef52b5eaf294221aa88a8c58391d38e6acce022
merilin13 <150146754+merilin13@users.noreply.github.com>
2023-12-14 14:53:18 +0200

    Add files via upload

---
+++ forloop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace forloop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Foe Loop");
+
+            int loop = 5;
+            for (int i = 0; i < loop; i++) 
+            {
+                Console.WriteLine("loop" + i); 
+                
+                Console.Beep();
+            }
+        }
+    }
+}
