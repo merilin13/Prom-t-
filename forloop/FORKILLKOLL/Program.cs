6262595ff2ba0269b1bb80b3dea4f4c8a3c08929
merilin13 <150146754+merilin13@users.noreply.github.com>
2023-12-18 13:03:25 +0200

    Add files via upload

---
+++ forloop/FORKILLKOLL/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FORKILLKOLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("kill-koll");
+                Console.WriteLine("kill-koll");
+                for (int j = 0; j < 1; j++) ;
+                {
+                  Console.WriteLine("killadi-koll");
+                    for (int k = 0; k < 2; k++) ;
+                    {
+                        Console.WriteLine("kill-koll");
+                        Console.WriteLine("kill-koll");
+                        for(int l = 0; l < 2; l++) ;
+                        {
+                            Console.WriteLine("killadi-koll");
+                        }
+                        Console.WriteLine("kill-koll");
+                    }
+                }
+            }
+        }
+    }
+}
