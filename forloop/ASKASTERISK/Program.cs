c3c15db5be30d825198267b9b02340532ca9e4ce
merilin13 <150146754+merilin13@users.noreply.github.com>
2023-12-18 12:32:35 +0200

    Add files via upload

---
+++ forloop/ASKASTERISK/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ASKASTERISK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta tärnide arv ->"); 
+            string nr = Console.ReadLine();
+            int muutuja = int.Parse(nr);
+            
+            for (int i = 0; i < muutuja; i++)
+            {
+                Console.Write("*");
+            }
+
+        }
+    }
+}
