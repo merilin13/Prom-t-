699fa6c7fb4c950bf84e229d3892f7d55800040c
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-01-08 13:37:13 +0200

    Add files via upload

---
+++ forlooptable/kolmnurk/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace kolmnurk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("kirjuta number");
+            int i = Convert.ToInt32(Console.ReadLine());
+            for (int j = 0; j <= i; j++)
+            {
+                for (int k = 0; k <= j; k++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine("\n");
+            }
+           
+
+
+        }
+    }
+}
