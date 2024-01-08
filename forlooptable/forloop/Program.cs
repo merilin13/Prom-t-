8e5e4760ea3937fc4f55e4546ac8d89230bfd4cf
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-01-08 12:53:55 +0200

    arvutused

---
+++ forlooptable/forloop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace forloop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Täisarvude kokku liitmine kuni kümneni");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("\n\n");
+            int j, sum = 0;
+            for (j = 1; j <= 10; j++)
+            {
+                sum = sum + j;
+                Console.WriteLine($"Summa on : " + sum);
+            }
+        }
+    }
+}
