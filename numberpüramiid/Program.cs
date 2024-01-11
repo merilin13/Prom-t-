222e9a30125e1b96e2a5760c323ddaaef6753744
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-01-11 15:28:13 +0200

    kahtlane püramiid

---
+++ numberpüramiid/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace numberpüramiid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+             Console.WriteLine("kirjuta number");
+            int i, j, n;
+            Console.Write("loo püramid **");
+            Console.WriteLine("sisesta suurus");
+            n = Convert.ToInt32(Console.ReadLine());
+            int Number = 0;
+            for (i = 0; i < n; i++)
+            {
+                for (j = 1; j < n-i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= 2*i-1 ; j++)
+                {
+                    Console.Write(Number++);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
