57ad0b6207614846a51a068bb31d301d26991c8d
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-02 13:29:05 +0200

    Add files via upload

---
+++ oddnumberforeach/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace oddnumberforeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] oddArray = new int [] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21 };
+
+           foreach (int num in oddArray)
+            {
+                Console.WriteLine(num);
+                if (num == 13)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
