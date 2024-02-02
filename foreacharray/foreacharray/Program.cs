315d0e3535c1205ab60f4573630522e486a96f20
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-02 13:09:54 +0200

    Add files via upload

---
+++ foreacharray/foreacharray/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace foreacharray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] genders = { "m", "f", "m", "m", "m", "f", "f", "m", "m","f"};
+              
+            int female = 0, male = 0;
+
+            foreach (string gender in genders) 
+            {
+                //if (gender == "m")
+                //{
+                //    male++;
+                //}
+                //if (gender == "f")
+                //{
+                //    female++;
+                //}
+
+                var result = gender == "m" ? male : female ;
+                
+            }
+
+            Console.WriteLine("number of female {0} and nr of male {1}", female, male);
+        }
+    }
+}
