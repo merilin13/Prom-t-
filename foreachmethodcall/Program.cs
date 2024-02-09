c0536e48bf9f9dbdad8b3e8527e28f7b30b3473d
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-09 12:43:04 +0200

    Add files via upload

---
+++ foreachmethodcall/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace foreachmethodcall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("punktide panek");
+            int[] marks = { 125, 132, 95, 116, 110 };
+
+            int highestMarks = Maximum(marks);
+
+            Console.WriteLine("the highest score is " + highestMarks);
+        }
+
+        public static int Maximum(int[] numbers)
+        {
+            int maxNumbers = numbers[0];
+
+            foreach(int number in numbers) 
+            {
+                if (number > maxNumbers)
+                {
+                    maxNumbers = number;
+                }
+            }
+            return maxNumbers;
+        }
+    }
+}
