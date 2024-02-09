4e339bdcbe9831ed89837a61a950349388653738
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-09 14:10:01 +0200

    Add files via upload

---
+++ foreachineveryway/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace foreachineveryway
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Foreach harjutused");
+            Console.WriteLine("1.list, kus on foreach");
+            int choice = Convert.ToInt16(Console.ReadLine());
+            if (choice == 1) 
+            {
+                ForeachWithList();
+            }
+            if (choice == 2)
+            {
+                Foreachanu();
+            }
+
+        }
+        static void ForeachWithList()
+        {
+          var names = new List <string>()
+            {"John", "Tom", "Anu", "Villu", "James"};
+            foreach (var name in names) 
+            { 
+                Console.WriteLine(name);
+            }
+        }
+
+        static void Foreachanu()
+        {
+            var tuuts = new List<string>()
+            { "Arvo", "Sav", "Tuut", "Piip", "Andero"};
+            foreach (var tuut in tuuts )
+            {
+                Console.WriteLine(tuut);
+            }
+
+        }
+    }
+}
