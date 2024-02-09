1f033adfbce0951f4b1ff54ca277d7febee49d8c
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-09 13:18:42 +0200

    Add files via upload

---
+++ autoforeach/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace autoforeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("autod");
+            string[] cars = { "a", "b", "c", "d" };
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+         
+    }
+}
