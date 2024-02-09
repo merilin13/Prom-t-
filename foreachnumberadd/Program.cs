8438c9484c98437cd5f7222e34acadcf0e7c32eb
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-09 13:34:30 +0200

    Add files via upload

---
+++ foreachnumberadd/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace foreachnumberadd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() {5, -8, 3, 14, 9, 17, 0, 4};
+
+            int sum = 0;
+
+            foreach(int i in numbers)
+            {
+                sum += i;
+
+            }
+
+            Console.WriteLine("Sum {0} ", sum);
+            Console.ReadLine();
+        }
+    }
+}
