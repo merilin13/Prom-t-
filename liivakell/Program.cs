217e9a034acf3ab6f56eed629481039b732f00b0
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-13 14:50:43 +0200

    Add files via upload

---
+++ liivakell/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace liivakell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, count = 1, number;
+            Console.Write("Enter number of rows:");
+            number = int.Parse(Console.ReadLine());
+
+            count = 1;
+            for (j = 1; j <= number - 1; j++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count++;
+                for (i = 1; i <= 2 * (number - j) - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            
+
+            count = number - 1;
+            for (j = 1; j <= number; j++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count--;
+                for (i = 1; i <= 2 * j - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            
+
+        }
+
+    }
+}
