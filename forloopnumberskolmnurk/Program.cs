7d08c50a5eeb88a92359f3c8e1773f1aeea71fd8
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-13 14:21:45 +0200

    Add files via upload

---
+++ forloopnumberskolmnurk/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace forloopnumberskolmnurk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+            Console.WriteLine("sisesta püramiidi suurus");
+            rows=Convert.ToInt32(Console.ReadLine());
+
+           
+
+            for(i = 1; i <= rows; i++)
+            {
+                for(j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
