c5fffed53a14610cbde7998665c3bd9e7eac668e
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-13 15:13:41 +0200

    Add files via upload

---
+++ forlooparray/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace forlooparray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array e jada e massiiv");
+
+            int[] array = new int[5];
+            array[0] = 10;
+            array[2] = 20;
+            array[4] = 30;
+             for (int i = 0; i < array.Length; i++ )
+            {
+
+                Console.WriteLine(array[i]);
+
+            }
+                      
+        }
+    }
+}
