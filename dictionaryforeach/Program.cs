5dc087d9637ede97b795d1e60265a421852da354
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-16 13:45:37 +0200

    Add files via upload

---
+++ dictionaryforeach/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace dictionaryforeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            
+            var domains = new Dictionary<string, string>()
+            {
+                {"est ","estonia"},
+                {"lat ","latvia" },
+                {"fin ","finland"},
+                {"swe ","sweeden"}
+
+               
+        };
+            int number = 1;
+            foreach (var item in domains)
+            {                  
+                    Console.WriteLine($"{item.Key} - {item.Value} - {number}"); 
+                number++;
+            }
+        }
+    }
+}
