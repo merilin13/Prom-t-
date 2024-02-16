5d758a490cd37eae39416855d95c334dcdecd248
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-16 12:28:16 +0200

    Add files via upload

---
+++ foreachnesting/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace foreachnesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("for each nesting");
+
+            var words = new List<List<string>>
+            {
+                new List<string> {"tea","coffee","jucie","smoothie"},
+                new List<string> {"cup","plate","knife","spoon"},
+                new List<string> {"rain","snow","hailstorm","sunny"}
+            };
+            //foreach on foreachi sees 
+            foreach (var word in words)
+            {
+                foreach (var List in word)
+                {
+                    Console.WriteLine(List);
+                }
+                
+            }
+        }
+    }
+}
