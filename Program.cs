103869998e8f5bf8b2a2e58a0bbdaa6ed7eecd48
opilane <opilane@HP-CZC0359NN9>
2024-02-23 11:24:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace listfor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var primenumbers = new List<int>();
+            primenumbers.Add(2);
+            primenumbers.Add(3);
+            primenumbers.Add(5);
+            primenumbers.Add(7);
+
+            Console.WriteLine("No of elelemts: " + primenumbers.Count);
+        }
+    }
+}
