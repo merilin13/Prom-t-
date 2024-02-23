0b8635c1b6850cb8a8533760a8fb047f9e77afa8
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-23 12:42:21 +0200

    Add files via upload

---
+++ foreachlist/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace foreachlist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>() { 10, 20, 30, 40, 50 };
+
+            intList.ForEach(el => Console.WriteLine(el));
+
+            foreach (var el in intList)
+            {
+                Console.WriteLine(el);
+            }
+
+            Console.WriteLine("------------------");
+            for (int i = 0; i < intList.Count; i++)
+            {
+                Console.WriteLine(intList[i]);
+            }
+        }
+    }
+}
