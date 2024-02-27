7f0fa8ffdce11a7dc5c43b6a7393b9bfa640fe8f
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-27 15:44:33 +0200

    Add files via upload

---
+++ ListNumberMinMaxSumAvarage/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace ListNumberMinMaxSumAvarage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 15, 16, 8, 6 };
+
+            Console.WriteLine(numbers.Max());
+            Console.WriteLine(numbers.Min());
+            Console.WriteLine(numbers.Sum());
+            Console.WriteLine(numbers.Average());
+
+            Console.WriteLine("-------------");
+            Console.WriteLine("sorteerib numbrid ära alates väiksemast suuremasse");
+            Array.Sort(numbers); 
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+           
+            Console.WriteLine("-------------");
+            Console.WriteLine("sorteerib numbrid ära alates suuremast väiksemasse");
+            Array.Reverse(numbers);
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("-------------");
+
+            Console.WriteLine(Array.BinarySearch(numbers, 15));
+        }
+    }
+}
