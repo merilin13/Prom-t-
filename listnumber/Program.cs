68b87ecacf955e897fe12aaff67563ecb603cb79
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-02-27 15:00:58 +0200

    Add files via upload

---
+++ listnumber/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace listnumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Massiiv e array");
+
+            int[] numbers = { 2, 4, 6, 8, 10 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("--------------------------------");
+          
+            for (int i = 0; i  < numbers.Length; i++) 
+            {
+                Console.WriteLine(numbers[i]+10);
+            }
+        }
+    }
+}
