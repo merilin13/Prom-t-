1a176fb1284f3f25c1fc3d01807326a359f44fa8
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-03-01 13:08:47 +0200

    Add files via upload

---
+++ arraysortname/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace arraysortname
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] animals = {"cat", "alligator", "fox", "donkey", "bear",
+                "elephant", "goat"};
+
+             //Array.Sort(animals);
+
+            foreach(string animal in animals) 
+            {
+                Console.WriteLine(animal);
+            }
+
+            Console.WriteLine("----------------------------");
+
+            Array.Sort(animals,0,3);
+            foreach (string animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+            Console.WriteLine("----------------------------");
+            int[] numbers = { 1, 2, 3, 4, 3, 55, 23, 2, };
+
+            IEnumerable<Product> noduplicates =
+    products.Distinct(new ProductComparer());
+
+            foreach (int number in numbers)
+            { 
+                Console.WriteLine(number); 
+            }
+        }
+    }
+}
