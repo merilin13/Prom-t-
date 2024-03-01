3122a8479f576b9fb50b413f2b8221d4ee7fe86f
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-03-01 12:26:17 +0200

    Add files via upload

---
+++ randomasteriks/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace randomasteriks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int stringRowmin = 0;   
+            int stringRowmax = 5;
+
+            int[] randomAsterisk = new int[stringRowmax];
+
+            Random asterisk = new Random();
+            for (int i = stringRowmin; i < randomAsterisk.Length; i++)
+            {
+                Console.WriteLine(string.Concat(Enumerable.Repeat("*",
+                    asterisk.Next(1,20))));
+            }
+            foreach (int i in randomAsterisk)
+            {
+                Console.WriteLine(string.Concat(Enumerable.Repeat("*",
+                   asterisk.Next(1, 20))));
+            }
+        }
+    }
+}
