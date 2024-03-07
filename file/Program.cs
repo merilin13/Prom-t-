0a72996ee1c4144957f802ae6e3cd0788da07931
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-03-07 10:28:54 +0200

    Add files via upload

---
+++ file/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kirjuta faili läbi konsooli");
+
+            string filepath = @"C:/Users/opilane/Desktop/Uus kaust/WriteToFile.txt";
+            string inputText = Console.ReadLine();
+
+            File.WriteAllText(filepath, inputText);
+        }
+    }
+}
