df0119b86898f1bbd614b9109bb7b5d407c4aaae
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-03-08 12:44:54 +0200

    Add files via upload

---
+++ luuletus/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace luuletus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ");
+
+            FileWordReading();
+        }
+
+
+        static void FileWordReading()
+        {
+            string line;
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:/Users/opilane/Desktop/luuletus.txt"))
+                {
+                    int cnt = 0;
+                    while (sr.EndOfStream == false)
+                    {
+                        ++cnt;
+                        line = sr.ReadLine();
+                        int pikkus = line.Length;
+                        Console.WriteLine(cnt + " " + line + " " + pikkus);
+                    }
+                    sr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Faili ei saa lugeda");
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+    
+}
