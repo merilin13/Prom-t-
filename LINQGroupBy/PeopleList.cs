7c6eb66be22032f620014b7da5af27d75834c53b
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-03-22 13:39:19 +0200

    Add files via upload

---
+++ LINQGroupBy/PeopleList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQGroupBy
+{
+    public class PeopleList
+    {
+        public static readonly List<People> peoples = new List<People>
+        {
+                new People()
+                {
+                    Id = 1,
+                    Name = "John",
+                    Age = 18,
+                    
+                },
+                new People()
+                {
+                    Id = 2,
+                    Name = "Steve",
+                    Age = 21,
+                    
+                },
+                new People() 
+                {
+                    Id = 3,
+                    Name = "Bill",
+                    Age = 18,
+                   
+                },
+                new People()
+                {
+                    Id = 4,
+                    Name = "Ram",
+                    Age = 20,
+                    
+                },
+                 new People()
+                {
+                    Id = 5,
+                    Name = "Abram",
+                    Age = 21,
+                    
+                }
+        };
+    }
+}
---
+++ LINQGroupBy/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Channels;
+
+namespace LINQGroupBy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            LINQGroupBy();
+            Console.WriteLine("------------------------------");
+            ToLookUp();
+
+        }
+        
+        static void LINQGroupBy()
+        {
+            {
+                var result = PeopleList.peoples
+                    .GroupBy(x => x.Age);
+
+                foreach (var age in result)
+                {
+                    Console.WriteLine("Vanus " + age.Key);  //Each group has a key 
+
+                    foreach (var p in age)  //Each group has a inner collection  
+                    {
+                        Console.WriteLine("Nimi " + p.Name);
+                    }
+                }
+            }
+        }
+        static void ToLookUp()
+        {
+            {
+                var result = PeopleList.peoples
+                    .ToLookup(x => x.Age);
+
+                foreach (var age in result)
+                {
+                    Console.WriteLine("Vanus " + age.Key);  //Each group has a key 
+
+                    foreach (var p in age)  //Each group has a inner collection  
+                    {
+                        Console.WriteLine("Nimi " + p.Name);
+                    }
+                }
+            }
+        }
+    }
+}
