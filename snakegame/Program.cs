f97e39942ca34cc19385fe0f78e6659ec6d09479
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-03-28 11:41:57 +0200

    Add files via upload

---
+++ snakegame/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading;
+namespace snakegame
+{
+    class Snake
+    {
+        int Height = 20;
+        int Width = 30;
+
+        int[] X = new int[50];
+        int[] Y = new int[50];
+
+        int fruitX;
+        int fruitY;
+
+        int parts = 3;
+
+        ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+        char key = 'W';
+
+        Random rnd = new Random();
+
+        Snake()
+        {
+            X[0] = 5;
+            Y[0] = 5;
+            Console.CursorVisible = false;
+            fruitX = rnd.Next(2, (Width - 2));
+            fruitY = rnd.Next(2, (Height - 2));
+        }
+        public void WriteBoard()
+        {
+            Console.Clear();
+            for (int i = 1; i <=(Width + 2) ;i++)
+            {
+                Console.SetCursorPosition(i, 1);
+                Console.Write("-");
+            }
+            for (int i = 1; i <= (Width + 2); i++)
+            {
+                Console.SetCursorPosition(i, (Height+2));
+                Console.Write("-");
+            }   
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.Write("|");
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition((Width +2),i);
+                Console.Write("|");
+            }
+        }
+        public void Input()
+        {
+            if (Console.KeyAvailable)
+            {
+                keyInfo = Console.ReadKey(true);
+                key = keyInfo.KeyChar;
+            }
+        }
+        public void WritePoint(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("#");
+        }
+
+        public void Logic()
+
+        {
+            if (X[0] == fruitX)
+            {
+                if (Y[0] == fruitY)
+                {
+                    parts++;
+                    fruitX = rnd.Next(2, (Width - 2));
+                    fruitY = rnd.Next(2, (Height - 2));
+                }
+            }
+            for (int i = parts; i > 1; i--)
+            {
+                X[i - 1] = X[i - 2];
+                Y[i - 1] = Y[i - 2];
+            }
+            switch (key)
+            {
+                case 'w':
+                    Y[0]--;
+                    break;
+
+                case 's':
+                    Y[0]++;
+                    break;
+                case 'd':
+                    X[0]++;
+                    break;
+                case 'a':
+                    Y[0]--;
+                    break;
+            }
+            for (int i = 0; i <=(parts-1); i++) 
+            {
+                WritePoint(X[i],Y[i]);
+                WritePoint(fruitX,fruitY);
+            }
+            Thread.Sleep(100);
+        }
+        static void Main(string[] args)
+        {
+           Snake snake = new Snake();
+            while(true)
+            {
+                snake.WriteBoard();
+                snake.Input();
+                snake.Logic();
+               
+            }
+            Console.ReadKey();
+        }
+    }
+  
+}   
