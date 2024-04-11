b9f4c8c51b76f2470aa9272f75d48d037878bba0
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-04-11 11:48:30 +0300

    Add files via upload

---
+++ pinpong/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace pinpong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Pong pong = new Pong(60, 20);
+            pong.Run();
+            Console.ReadKey();
+        }
+    }
+}
